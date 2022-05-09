1b398286270fcca434e6fc2ea677057a08c55001
Come Wasik <wasik.come@gmail.com>
2022-05-09 12:07:01 +0200

    add relationships

--- api/models/index.js
+++ api/models/index.js
@@ -7,3 +7,11 @@ const Transaction = require('./Transaction');
 // Define that Car has one User and User has many Cars
 User.hasMany(Car);
 Car.belongsTo(User);
+
+// Define that User has many Transactions and Transaction has one User
+User.hasMany(Transaction);
+Transaction.belongsTo(User)
+
+// Define that a Transaction has one Car and Car has many Transactions
+Car.hasMany(Transaction);
+Transaction.belongsTo(Car);
