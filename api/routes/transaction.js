b347e42ecd9af485fc14e1cdc33d353ca216aa57
sassank <sassan.khalafi@gmail.com>
2022-05-10 10:39:41 +0200

    add transaction route

---
+++ api/routes/transaction.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express();
+const Transaction = require("../database//models/Tr");
+
+// Set RESTFUL api routes for sequilize model Transaction
+router.get("/", (req, res) => {
+    Transaction.findAll().then(Transactions => {
+        res.json(Transactions).status(200); // 200 = OK
+    }
+    ).catch(err => {
+        res.json(err).status(500); // 500 = Internal Server Error
+    }
+    );
+});
+
+// Set post route
+router.post("/", (req, res) => {
+    Transaction.create(req.body).then(Transaction => {
+        res.json(Transaction).status(201); // 201 = Created
+    }
+    ).catch(err => {
+        res.json(err).status(500); // 500 = Internal Server Error
+    }
+    );
+});
+
+// Set put route
+router.put("/:id", (req, res) => {
+    Transaction.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(Transaction => {
+        res.json(Transaction).status(200); // 200 = OK
+    }
+    ).catch(err => {
+        res.json(err).status(500); // 500 = Internal Server Error
+    }
+    );
+});
+
+// Set delete route
+router.delete("/:id", (req, res) => {
+    Transaction.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(Transaction => {
+        res.json(Transaction).status(204); // 204 = No content
+    }
+    ).catch(err => {
+        res.json(err).status(500); // 500 = Internal Server Error
+    }
+    );
+});
+
+module.exports = router;
